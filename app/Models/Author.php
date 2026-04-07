47c237c660a5ac07b28a572fd1649865c44c257d
WalterMijangos <walter24mijangosp@gmail.com>
2026-04-07 15:53:05 -0600

    Creacion de modelo
    A los modelos se les modifico añadiéndoles las relaciones entre el modelo de autor y el modelo de libro

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
-    //
+    protected $fillable = [
+        'name'
+    ];
+
+    //esta funcion es para relacionar el autor con los libros quie se vayan a colocar en la vista
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
 }
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected $fillable =[
+        'title',
+        'year',
+        'author_id'
+    ];
+
+    //esta funcion es para relacionar el libro con el autor que se va a colocar en el formulario.
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
 }
